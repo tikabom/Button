59f4e557ab0ec8289ed7cc7f5312169e2c4a1689
Avantika Iyer <avantika.hiyer@gmail.com>
2018-05-14 18:06:19 -0400

    update entry

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-import App from './components/app';
+import App from './app';
 
-ReactDom.render(<App/>, document.querySelector('#app'));
+ReactDom.render(
+  (<BrowserRouter>
+    <App />
+  </BrowserRouter>), document.querySelector('#app'));

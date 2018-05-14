7a4ffb5b689d608ae04d69fec02eae1e683d929d
Avantika Iyer <avantika.hiyer@gmail.com>
2018-05-14 02:10:39 -0400

    add packages and webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: "./src/index.js",
+  module: {
+    loaders: [
+      {
+        test: /\.js|.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-class-properties',
+                     'transform-decorators-legacy']
+        }
+      }
+    ]
+  },
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  }
+};

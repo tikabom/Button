de2ea94e5ec97ce86f83f2ad5ffab4fbbc4e5ebd
Avantika Iyer <avantika.hiyer@gmail.com>
2018-05-16 00:08:11 -0400

    add routes

--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Candidate from './components/candidate';
-import User from './components/user';
+import UserView from './components/user';
+import TransferView from './components/transfer';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Route exact path='/' component={Candidate} />
-        <Route path='/user' component={User} />
+        <Route path='/users' component={UserView} />
+        <Route path='/transfers' component={TransferView} />
       </div>
     );
   }
